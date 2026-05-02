a1e69c0fc6a9cf4f816fdfa34703a246a3ed6044
mais-arman <maisarman122@gmail.com>
2026-05-02 20:26:58 +0300

    Add Docker development setup with PostgreSQL

--- apps/backend/app/__init__.py
+++ apps/backend/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from app.config import Config
+from app.extensions import db, migrate
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    return app
+    db.init_app(app)
+    migrate.init_app(app, db)
 
+    return app
--- apps/backend/app/config.py
+++ apps/backend/app/config.py
@@ -7,3 +7,5 @@ class Config:
     ENV = os.getenv("FLASK_ENV")
     DEBUG = ENV == "development"
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
